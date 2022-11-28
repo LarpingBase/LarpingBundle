018131622b6fb1e73d1c1b8768763e2f288c773f
Ruben van der Linde <ruben@conduction.nl>
2022-11-28 15:05:23 +0100

    First installation example

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # LarpingBundle
 An example repository for creating php symfony flex bundles
 
-This bundle is now doable, maar jhet kon toch nog beter
+Used to test versioning
--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -1,30 +1,36 @@
 <?php
 
 // src/Service/LarpingService.php
-
 namespace LarpingBase\LarpingBundle\Service;
 
 use App\Entity\DashboardCard;
+use App\Entity\Endpoint;
+use CommonGateway\CoreBundle\Installer\InstallerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-class InstallationService
+class InstallationService implements InstallerInterface
 {
     private EntityManagerInterface $entityManager;
+    private SymfonyStyle $io;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
 
-    public function install(){
+    public function install(SymfonyStyle $io){
+        $this->io = $io;
         $this->checkDataConsistency();
     }
 
-    public function update(){
+    public function update(SymfonyStyle $io){
+        $this->io = $io;
         $this->checkDataConsistency();
     }
 
-    public function uninstall(){
+    public function uninstall(SymfonyStyle $io){
+        $this->io = $io;
         // Do some cleanup
     }
 
@@ -34,15 +40,45 @@ class InstallationService
         $objectsThatShouldHaveCards = ['https://larping.nl/character.schema.json','https://larping.nl/skill.schema.json'];
 
         foreach($objectsThatShouldHaveCards as $object){
-            echo $object;
+            $this->io->writeln('Looking for a dashboard card for: '.$object);
+            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference'=>$object]);
+            if(
+               !$dashboardCard = $this->entityManager->getRepository('App:DashboardCard')->findOneBy(['entityId'=>$entity->getId()])
+            ){
+                $dashboardCard = New DashboardCard();
+                $dashboardCard->setType('schema');
+                $dashboardCard->setEntity('App:Entity');
+                $dashboardCard->setObject('App:Entity');
+                $dashboardCard->setName($entity->getName());
+                $dashboardCard->setDescription($entity->getDescription());
+                $dashboardCard->setEntityId($entity->getId());
+                $dashboardCard->setOrdering(1);
+                $this->entityManager->persist($dashboardCard);
+                $this->io->writeln('Dashboard card created');
+                continue;
+            }
+            $this->io->writeln('Dashboard card found');
         }
 
         // Let create some endpoints
         $objectsThatShouldHaveEndpoints = ['https://larping.nl/character.schema.json','https://larping.nl/skill.schema.json','https://larping.nl/story.schema.json'];
 
         foreach($objectsThatShouldHaveEndpoints as $object){
-            echo $object;
+            $this->io->writeln('Looking for a endpoint card for: '.$object);
+            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference'=>$object]);
+
+            if(
+                count($entity->getEndpoints()) == 0
+            ){
+                $endpoint = New Endpoint($entity);
+                $this->entityManager->persist($endpoint);
+                $this->io->writeln('Endpoint created');
+                continue;
+            }
+            $this->io->writeln('Endpoint found');
         }
+
+        $this->entityManager->flush();
 
         // Lets see if there is a generic search endpoint
 
