818e11c65dee82fc2022490864322af52ef80f68
Ruben van der Linde <ruben@conduction.nl>
2023-01-18 15:55:50 +0100

    Add modiviers to abiility scores

--- Service/LarpingService.php
+++ Service/LarpingService.php
@@ -4,15 +4,56 @@
 
 namespace LarpingBase\LarpingBundle\Service;
 
+use App\Entity\ObjectEntity;
+
 class LarpingService
 {
     /*
-     * Returns a welcoming string
+     * Calculates the atribute when an characters is changed
      *
      * @return array
      */
-    public function petStoreHandler(array $data, array $configuration): array
+    public function AtributeHandler(array $data, array $configuration): array
     {
+
+        $attributes = [];
+
+        foreach($character->getValue("skills") as $skill){
+
+        }
+
         return ['response' => 'Hello. Your LarpingBundle works'];
     }
+
+    private function calculateAbillities(array $abillities, ObjectEntity $effect): array{
+
+        // What if the abbility has not been added yet
+        if(!in_array($effect->getValue('abillity'), $abillities)){
+            $abillities[$effect->getValue('abillity')->getId()] =
+                [
+                    "name" => $effect->getValue('abillity')->getValue('name'),
+                    "value" => $effect->getValue('abillity')->getValue('base'),
+                    "effects" => []
+                ];
+        }
+
+        // Get current vallue
+        $value = $abillities[$effect->getValue('abillity')->getId()]["value"];
+
+        if($effect->getValue('positive')){
+            $value = $value + $effect->getValue('modifier');
+            $effectDescription = "(+ ".$effect->getValue('modifier').") ".$effect->getValue('name');
+        }
+        else{
+            $value = $value - $effect->getValue('modifier');
+            $effectDescription = "(- ".$effect->getValue('modifier').") ".$effect->getValue('name');
+        }
+
+        // Set the calculated effects
+        $abillities[$effect->getValue('abillity')->getId()]["value"] = $value;
+        $abillities[$effect->getValue('abillity')->getId()]["effects"] = $effectDescription;
+
+        return $abillities;
+    }
+
 }
