29a79d96f59226df949cc39a748918bd325093b5
Ruben van der Linde <ruben@conduction.nl>
2023-03-21 14:04:37 +0100

    Small config changes for linting

--- DependencyInjection/LarpingExtension.php
+++ DependencyInjection/LarpingExtension.php
@@ -9,9 +9,17 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class LarpingExtension extends Extension
 {
+    /**
+     * @param array $configs The configuration
+     * @param ContainerBuilder $container The container
+     * @return void
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
-    }
-}
+
+    }//end load()
+
+
+}//end class
